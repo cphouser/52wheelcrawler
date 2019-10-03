fab39f5afc98d54375854b9f12a4cbedaab80a1f
Calvin Houser <cphouser@gmail.com>
2019-10-03 08:52:25 +0000

    compares results to reference csv, specify date for end of search in cmd line
    does not move to next page correctly, date check and postive match untested

--- part_search.py
+++ part_search.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from pyvirtualdisplay import Display
 import csv
+import sys
+import datetime
 
 def init():
     display = Display(visible=0, size=(800, 600))
@@ -14,17 +16,58 @@ def init():
 
 def main():
     driver = init()
+    reference_dict = load_car_table('cars.csv')
+    driver.get('https://row52.com/Search/Index?Page=1&MakeId=0&ModelId=0&Year=&Distance=50&Sort=dateadded&SortDirection=desc&ZipCode=95403&HasImage=&HasComment=&LocationId=&V1=&V2=&V3=&V4=&V5=&V6=&V7=&V8=&V9=&V10=&V11=&V12=&V13=&V14=&V15=&V16=&V17=&IsVin=false')
+    end_date = datetime.datetime.strptime(sys.argv[1],'%m/%d/%y')
+    while True:
+        result_tups, more = results_from_page(driver, reference_dict, end_date)
+        if more is False:
+            break
+        next_page(driver)
+    driver.quit()
+
+def results_from_page(driver, reference_dict, end_date):
     result_list = fetch_page(driver)
     result_tups = [clean_result(result) for result in result_list]
-    reference_dict = load_car_table('cars.csv')
     #for make in reference_dict:
     #    print(make)
     #    for model in reference_dict[make]:
     #        print('--', *model, sep='\t')
     #    print('...')
+    matching_results = []
     for result in result_tups:
-        print(result)
-    driver.quit()
+        list_date = datetime.datetime.strptime(result[1], '%b %d, %Y')
+        if list_date < end_date:
+            return (matching_results, False)
+        #result = result_tups[i]
+        matches = check_matches(result, reference_dict)
+        for match in matches:
+            print(match)
+        else: print("no matches for", result)
+        if len(matches) > 0:
+            result.append(matching_results)
+    return (matching_results, True)
+        
+def check_matches(listing, ref_dict):
+    year, make, model = listing[0]
+    make = make.upper()
+    model = model.upper()
+    if ref_dict.get(make) is None: return []
+    else:
+        ref_makes = ref_dict[make]
+        matches = []
+        for ref_make in ref_makes:
+            make_words = make.split()
+            if all([ref_make[0].find(word) > 0 for word in make_words]) \
+                    and in_year_range(year, ref_make[1], ref_make[2]):
+                matches.append(ref_make)
+                print(ref_make)
+        return matches
+
+def in_year_range(year, min_y, max_y):
+    if int(year) >= min_y and int(year) <= max_y: return True
+    else: return False
+
 
 def load_car_table(filen):
     car_dict = {}
@@ -40,8 +83,11 @@ def load_car_table(filen):
                 car_dict.update({name[0]: current_item})
     return car_dict
 
+def next_page(driver):
+    next_btn = driver.find_element_by_xpath("//input[@value='NEXT']")
+    next_btn.submit()
+
 def fetch_page(driver):
-    driver.get('https://row52.com/Search/Index?Page=1&MakeId=0&ModelId=0&Year=&Distance=50&Sort=dateadded&SortDirection=desc&ZipCode=95403&HasImage=&HasComment=&LocationId=&V1=&V2=&V3=&V4=&V5=&V6=&V7=&V8=&V9=&V10=&V11=&V12=&V13=&V14=&V15=&V16=&V17=&IsVin=false')
     result_list = driver.find_elements_by_class_name('list-row')
     print('page retrieved')
     return(result_list)
